25b8273b6884ba98961d8c8ecb06aa000d2a3e2f
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-09-11 15:05:41 -0300

    Add files via upload

---
+++ conteudo/js/ReleaseScript.js
@@ -0,0 +1,85 @@
+// script.js
+
+
+
+const materialidadeCheckbox = document.getElementById("materialidadeCheckbox");
+const materialidadeLabel = document.querySelector("label[for='materialidade']");
+const materialidadeTextarea = document.getElementById("materialidade");
+
+materialidadeCheckbox.checked = false;
+materialidadeTextarea.style.display = "none";
+materialidadeTextarea.disabled = true;
+
+materialidadeCheckbox.addEventListener("change", function () {
+  if (materialidadeCheckbox.checked) {
+    materialidadeTextarea.style.display = "block";
+    materialidadeTextarea.disabled = false;
+  } else {
+    materialidadeTextarea.style.display = "none";
+    materialidadeTextarea.disabled = true;
+  }
+});
+
+function copiarCampos() {
+  var fato = document.getElementById("fato").value;
+  var cidade = document.getElementById("cidade").value;
+  var dataInput = document.getElementById("data");
+  var hora = document.getElementById("hora").value;
+  var detidos = document.getElementById("detidos").value;
+  var endereco = document.getElementById("endereco").value;
+  var historico = document.getElementById("historico").value;
+  var materialidadeCheckbox = document.getElementById("materialidadeCheckbox").checked;
+  var mensagem = formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, materialidadeCheckbox);
+
+  // Criar um elemento temporário e definir seu valor como a mensagem
+  var temp = document.createElement("textarea");
+  temp.value = mensagem;
+
+  // Adicionar o elemento temporário à página
+  document.body.appendChild(temp);
+
+  // Selecionar o conteúdo do elemento temporário
+  temp.select();
+
+  // Copiar o conteúdo selecionado para a área de transferência
+  document.execCommand("copy");
+
+  // Remover o elemento temporário da página
+  document.body.removeChild(temp);
+
+  // Exibir um alerta de sucesso
+  Swal.fire({
+    confirmButtonColor: "#694f43",
+    icon: 'success',
+    title: 'COPIADO!',
+    iconColor: '#7c7565',
+    confirmButtonText: 'OK'
+}).then((result) => {
+    if (result.isConfirmed) {
+        window.open('https://api.whatsapp.com/', '_blank');
+    }
+});
+}
+
+function formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, mostrarMaterialidade) {
+  const rawDate = dataInput.value;
+  const date = new Date(rawDate);
+  const timezoneOffset = date.getTimezoneOffset() / 60;
+  date.setUTCHours(date.getUTCHours() + timezoneOffset);
+  const formattedDate = ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
+  const dataText = "" + formattedDate;
+  const materialidade = document.getElementById("materialidade").value;
+  const materialidadeText = mostrarMaterialidade ? `\n*MATERIALIDADE APREENDIDA:* \n${materialidade}\n` : "";
+  const mensagem = `*FATO:* ${fato}\n*DATA:* ${dataText}\n*HORA:* ${hora}\n*ENDEREÇO:* ${cidade}, ${endereco}\n\n*HISTÓRICO:* \n${historico}\n${materialidadeText}\n*INDIVÍDUOS DETIDOS:* \n${detidos}`;
+  return mensagem.toUpperCase(); // Converter a mensagem inteira para maiúsculas
+}
+
+
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+const form = document.querySelector("form");
+
+themeToggle.addEventListener("click", function() {
+  body.classList.toggle("darkmode");
+  form.classList.toggle("darkmode");
+});
