c377b93cfef7af7bd9d6a85948910bf066df9e02
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-09-12 01:45:12 -0300

    Update sw.js

--- conteudo/js/sw.js
+++ conteudo/js/sw.js
@@ -2,10 +2,17 @@
 const cacheName = 'minha-pagina-cache-v1';
 const cacheFiles = [
     'https://190alertas.github.io/home',
-    '/index.html',
-    '/main.js',
-    '/styles.css', // Se você tiver um arquivo CSS
-    '/imagens/logo.png', // Se você tiver imagens
+    'https://190alertas.github.io/home/conteudo/release',
+    'https://190alertas.github.io/home/conteudo/veiculos',
+    'https://190alertas.github.io/home/conteudo/assuntos',
+    'https://190alertas.github.io/home/conteudo/feedback',
+    'https://190alertas.github.io/home/conteudo/js/main.js',
+    'https://190alertas.github.io/home/conteudo/js/ReleaseScript.js',
+    'https://190alertas.github.io/home/conteudo/js/manifest.json',
+    'https://190alertas.github.io/home/conteudo/js/AssuntosScript.js',
+    'https://190alertas.github.io/home/conteudo/js/VeiculoScript.js',
+    'https://190alertas.github.io/home/conteudo/css/theme.css', // Se você tiver um arquivo CSS
+    'https://raw.githubusercontent.com/190ALERTAS/190/main/conteudo/img/Logo%20190ALERTAS.png', // Se você tiver imagens
 ];
 
 self.addEventListener('install', function(event) {
