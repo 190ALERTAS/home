cd813d147470049542aa31d530cba24ab5e6ce91
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-09-12 01:47:15 -0300

    Update main.js

--- conteudo/js/main.js
+++ conteudo/js/main.js
@@ -1,6 +1,6 @@
 // Verifique se o Service Worker é suportado pelo navegador
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('https://190alertas.github.io/home/conteudo/js/sw.js')
         .then(function(registration) {
             console.log('Service Worker registrado com sucesso:', registration);
         })
