3cf6fe4ec09285b23307c1bab7315ef4923f95b8
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-09-13 14:15:20 -0300

    Update VeiculoScript.js

--- conteudo/js/VeiculoScript.js
+++ conteudo/js/VeiculoScript.js
@@ -55,7 +55,20 @@ const themeToggle = document.getElementById("themeToggle");
 const body = document.body;
 const form = document.querySelector("form");
 
-  themeToggle.addEventListener("click", function() {
+// Verifique se o tema escuro já está definido no localStorage
+if (localStorage.getItem('darkTheme') === 'enabled') {
+  body.classList.add('darkmode');
+  form.classList.add('darkmode');
+}
+
+themeToggle.addEventListener("click", function() {
   body.classList.toggle("darkmode");
   form.classList.toggle("darkmode");
-  });
+
+  // Armazene a preferência do usuário no localStorage
+  if (body.classList.contains('darkmode')) {
+    localStorage.setItem('darkTheme', 'enabled');
+  } else {
+    localStorage.setItem('darkTheme', 'disabled');
+  }
+});
