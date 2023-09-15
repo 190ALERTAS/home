35fba5bbf02d6a28c4f3b002581f6ea0038b7647
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-09-15 10:19:18 -0300

    Alteração de tema
    -Inclusão do iframe na alternância de cor, agora a guia "Sugestões" também altera o tema

--- js/base.js
+++ js/base.js
@@ -1,16 +1,29 @@
 function toggleDarkMode() {
   const body = document.body;
   const icon = document.getElementById('icon');
+  const iframes = document.querySelectorAll('iframe'); // Seleciona todos os iframes
+
   if (body.classList.contains('dark-mode')) {
-      body.classList.remove('dark-mode');
-      icon.src = 'https://raw.githubusercontent.com/190ALERTAS/home/main/img/sun-solid-24.png'; // Altere para o ícone do sol
-      localStorage.setItem('darkMode', 'false'); // Salve a preferência como "false"
+    body.classList.remove('dark-mode');
+    icon.src = 'https://raw.githubusercontent.com/190ALERTAS/home/main/img/sun-solid-24.png';
+    localStorage.setItem('darkMode', 'false');
+    
+    // Remove a classe dark-mode dos iframes quando o tema claro estiver ativado
+    iframes.forEach(iframe => {
+      iframe.classList.remove('dark-mode');
+    });
   } else {
-      body.classList.add('dark-mode');
-      icon.src = 'https://raw.githubusercontent.com/190ALERTAS/home/main/img/moon-solid-24.png'; // Altere para o ícone da lua
-      localStorage.setItem('darkMode', 'true'); // Salve a preferência como "true"
+    body.classList.add('dark-mode');
+    icon.src = 'https://raw.githubusercontent.com/190ALERTAS/home/main/img/moon-solid-24.png';
+    localStorage.setItem('darkMode', 'true');
+    
+    // Adiciona a classe dark-mode aos iframes quando o tema escuro estiver ativado
+    iframes.forEach(iframe => {
+      iframe.classList.add('dark-mode');
+    });
   }
 }
+
 // Verificar se o usuário já tem uma preferência salva no localStorage
 const storedDarkMode = localStorage.getItem('darkMode');
 if (storedDarkMode === 'true') {
