74f618017dbed77c2c52d54f9319158a2cee8bc2
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2023-10-20 00:00:07 -0300

    correção para touch

--- js/EscalaScript.js
+++ js/EscalaScript.js
@@ -57,6 +57,16 @@
             a.click();
         });
 
+        salvar.addEventListener("touchend", function() {
+            const dataToSave = JSON.stringify(registros);
+            const blob = new Blob([dataToSave], { type: "application/json" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "registros.json";
+            a.click();
+        });
+
         importar.addEventListener("change", function(event) {
             const file = event.target.files[0];
             const reader = new FileReader();
