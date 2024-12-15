ca10f8b4d4249c95f1f674e0886924724dea2948
190ALERTAS <144716759+190ALERTAS@users.noreply.github.com>
2024-12-15 09:32:52 -0300

    v 3.40
    Alteração significativa nos itens do Release.
    - Adicionado Batalhão/CIA
    - Adicionado "Inserir dados adicionais"
    - Itens separados por vírgula viram itens separados
    - Histórico organizado para o fim do layout

--- js/ReleaseScript.js
+++ js/ReleaseScript.js
@@ -16,64 +16,56 @@ materialidadeCheckbox.addEventListener("change", function () {
   }
 });
 
-function copiarCampos() {
-  var fato = document.getElementById("fato").value;
-  var cidade = document.getElementById("cidade").value;
-  var dataInput = document.getElementById("data");
-  var hora = document.getElementById("hora").value;
-  var detidos = document.getElementById("detidos").value;
-  var endereco = document.getElementById("endereco").value;
-  var historico = document.getElementById("historico").value;
-  var materialidadeCheckbox = document.getElementById("materialidadeCheckbox").checked;
-  var mensagem = formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, materialidadeCheckbox);
-
-  // Criar um elemento temporário e definir seu valor como a mensagem
-  var temp = document.createElement("textarea");
-  temp.value = mensagem;
-
-  // Adicionar o elemento temporário à página
-  document.body.appendChild(temp);
-
-  // Selecionar o conteúdo do elemento temporário
-  temp.select();
-
-  // Copiar o conteúdo selecionado para a área de transferência
-  document.execCommand("copy");
-
-  // Remover o elemento temporário da página
-  document.body.removeChild(temp);
-
-  // Registre o evento no Google Analytics
-  gtag('event', 'copiar_release', {
-    event_category: 'Botão',
-    event_label: 'Clique em Copiar Release',
-  });
-
-  // Exibir um alerta de sucesso
-  Swal.fire({
-    confirmButtonColor: "#694f43",
-    icon: 'success',
-    title: 'COPIADO!',
-    iconColor: '#7c7565',
-    confirmButtonText: 'OK'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      window.open('https://api.whatsapp.com/', '_blank');
-    }
-  });
-}
-
-function formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, mostrarMaterialidade) {
-  const rawDate = dataInput.value;
+function formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, mostrarMaterialidade, batalhao, ba, dp, vtr, gu) {
+  const rawDate = dataInput;
   const date = new Date(rawDate);
+
+  // Processar timezone
   const timezoneOffset = date.getTimezoneOffset() / 60;
   date.setUTCHours(date.getUTCHours() + timezoneOffset);
+
+  // Formatar a data no formato dd/mm/yyyy
   const formattedDate = ("0" + date.getDate()).slice(-2) + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
   const dataText = "" + formattedDate;
+
+  // Processar materialidade
   const materialidade = document.getElementById("materialidade").value;
-  const materialidadeText = mostrarMaterialidade ? `\n*MATERIALIDADE APREENDIDA:* \n${materialidade}\n` : "";
-  const mensagem = `*FATO:* ${fato}\n*DATA:* ${dataText}\n*HORA:* ${hora}\n*ENDEREÇO:* ${cidade}, ${endereco}\n\n*HISTÓRICO:* \n${historico}\n${materialidadeText}\n*INDIVÍDUOS DETIDOS:* \n${detidos}`;
-  return mensagem.toUpperCase(); // Converter a mensagem inteira para maiúsculas
+  const materialidadeList = materialidade.split(",").map(item => `- ${item.trim()}`).join("\n");
+  const materialidadeText = mostrarMaterialidade && materialidadeList ? `\n\n*APREENSÃO:* \n${materialidadeList}` : "";
+
+  // Processar indivíduos detidos
+  const detidosList = detidos
+    ? detidos.split(",").filter(individuo => individuo.trim() !== "").map(individuo => `- ${individuo.trim()}`).join("\n")
+    : "";
+  const detidosText = detidosList
+    ? `\n*INDIVÍDUOS DETIDOS:* \n${detidosList}\n`
+    : "\n*INDIVÍDUOS DETIDOS:* \n- Nenhum\n";
+
+  // Campos adicionais (apenas se houver valor)
+  const baText = ba ? `*BA:* ${ba}` : "";
+  const dpText = dp ? `*DP:* ${dp}` : "";
+  const vtrText = vtr ? `\n*VTR:* ${vtr}` : "";
+  const guText = gu ? `*GU:* ${gu}` : "";
+
+  // Combinar os campos adicionais com espaçamento condicional
+  const camposAdicionais = [baText, dpText, vtrText, guText].filter(text => text !== "").join("    ");
+
+  // Formatar a mensagem
+  const mensagem = `🚔${batalhao}🚔
+
+*FATO:* ${fato}
+*DATA:* ${dataText}
+*HORA:* ${hora}
+*ENDEREÇO:* ${cidade}, ${endereco}
+${materialidadeText}
+${detidosText}
+*HISTÓRICO:*
+${historico}
+
+${camposAdicionais}`;
+
+  // Converter a mensagem inteira para maiúsculas
+  return mensagem.toUpperCase();
 }
 
 function toggleDarkMode() {
@@ -101,4 +93,106 @@ const storedDarkMode = localStorage.getItem('darkMode');
 if (storedDarkMode === 'true') {
   // Se a preferência for "true", ativar o modo escuro
   toggleDarkMode();
+}
+
+document.getElementById('btnInserirDados').addEventListener('click', () => {
+  const formContent = document.getElementById('formTemplate').innerHTML;
+
+  Swal.fire({
+    showCancelButton: true,
+    title: 'Inserir Dados Adicionais',
+    cancelButtonText: "Cancelar",
+    confirmButtonColor: "#694f43",
+    html: formContent,
+    focusConfirm: false,
+    preConfirm: () => {
+      const dados = {
+        ba: document.getElementById('ba').value,
+        dp: document.getElementById('dp').value,
+        vtr: document.getElementById('vtr').value,
+        gu: document.getElementById('gu').value
+      };
+
+      if (dados.ba || dados.dp || dados.vtr || dados.gu) {
+        document.getElementById('loadingPage').style.display = 'block';
+        document.getElementById('statusDadosInseridos').innerText = "Dados Adicionais Inseridos ✅";
+
+        // Armazenar os dados temporariamente para serem copiados depois
+        window.dadosAdicionais = dados;
+        console.log('Dados armazenados:', window.dadosAdicionais);
+      } else {
+        Swal.showValidationMessage('Preencha ao menos um campo');
+      }
+    }
+  });
+});
+
+// Função para limpar os dados e resetar o status
+function limparDados() {
+  // Resetar o status na página
+  document.getElementById('statusDadosInseridos').innerText = "Nenhum dado inserido";
+  document.getElementById('loadingPage').style.display = 'none';
+
+  // Limpar os dados armazenados globalmente
+  window.dadosAdicionais = null;
+
+  console.log('Dados limpos:', window.dadosAdicionais);
+}
+
+// Adicionar evento ao botão de limpar dados
+document.getElementById('btnLimparDados').addEventListener('click', limparDados);
+
+function copiarCampos() {
+  // Coletar os dados inseridos anteriormente
+  var fato = document.getElementById("fato") ? document.getElementById("fato").value : '';
+  var cidade = document.getElementById("cidade") ? document.getElementById("cidade").value : '';
+  var dataInput = document.getElementById("data") ? document.getElementById("data").value : '';
+  var hora = document.getElementById("hora") ? document.getElementById("hora").value : '';
+  var detidos = document.getElementById("detidos") ? document.getElementById("detidos").value : '';
+  var endereco = document.getElementById("endereco") ? document.getElementById("endereco").value : '';
+  var historico = document.getElementById("historico") ? document.getElementById("historico").value : '';
+  var materialidadeCheckbox = document.getElementById("materialidadeCheckbox") ? document.getElementById("materialidadeCheckbox").checked : false;
+  var batalhao = document.getElementById("batalhao") ? document.getElementById("batalhao").value : '';
+  var ba = window.dadosAdicionais?.ba || ''; // Coletando dados adicionais armazenados
+  var dp = window.dadosAdicionais?.dp || ''; // Coletando dados adicionais armazenados
+  var vtr = window.dadosAdicionais?.vtr || ''; // Coletando dados adicionais armazenados
+  var gu = window.dadosAdicionais?.gu || ''; // Coletando dados adicionais armazenados
+
+  // Verificar se algum campo necessário está vazio antes de copiar
+  if (!cidade || !dataInput || !hora || !endereco) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Erro',
+      text: 'Verifique o endereço, cidade, data e hora!',
+    });
+    return;  // Interrompe a execução se faltar dados importantes
+  }
+
+  // Formatar a mensagem a ser copiada
+  var mensagem = formatMessage(dataInput, hora, endereco, historico, fato, cidade, detidos, materialidadeCheckbox, batalhao, ba, dp, vtr, gu);
+
+  // Criar um elemento temporário para copiar a mensagem
+  var temp = document.createElement("textarea");
+  temp.value = mensagem;
+
+  // Adicionar o elemento temporário à página
+  document.body.appendChild(temp);
+
+  // Selecionar o conteúdo do elemento temporário
+  temp.select();
+
+  // Copiar o conteúdo para a área de transferência
+  document.execCommand("copy");
+
+  // Remover o elemento temporário da página
+  document.body.removeChild(temp);
+
+  // Exibir um alerta de sucesso
+  Swal.fire({
+    iconColor: '#7c7565',
+    icon: "success",
+    confirmButtonColor: "#694f43",
+    title: "Texto Copiado",
+    footer: '<a href="https://api.whatsapp.com/" target="_blank">Abrir Whatsapp</a>'
+  });
 }
